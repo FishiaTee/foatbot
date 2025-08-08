6f449f63f86d0c2705169f87736b41745fddbbfd
Fishia <74685931+FishiaTee@users.noreply.github.com>
2025-08-08 10:30:25 +0700

    basic data loading

--- data/impl/basic.py
+++ data/impl/basic.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pathlib
 from .. import data_handler
 
 class BasicDataHandler(data_handler.DataHandler):
@@ -10,3 +11,10 @@ class BasicDataHandler(data_handler.DataHandler):
             json.dump(self.data['users'][user], open(os.path.join(self.working_directory, "users", f"{str(user)}.json"), 'w'), indent=2)
         json.dump(self.data['server'], open(os.path.join(self.working_directory, "general.json"), 'w'), indent=2)
         return super().write_to_disk()
+    def read_from_disk(self):
+        for file in os.listdir(os.path.join(self.working_directory, "users")):
+            user_data = json.load(open(os.path.join(self.working_directory, "users", file)))
+            self.data['users'][pathlib.Path(os.path.join(self.working_directory, "users", file)).stem] = user_data
+        server_data = json.load(open(os.path.join(self.working_directory, "general.json")))
+        self.data['server'] = server_data
+        return super().read_from_disk()
